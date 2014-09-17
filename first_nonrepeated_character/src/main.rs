2dc68b748aa10753876c8214f9d35559e221016f
Sean Chen <skypemaster007@gmail.com>
2014-09-17 14:20:20 -0700

    Refactored first_nonrepeated_character function

--- first_nonrepeated_character/src/main.rs
+++ first_nonrepeated_character/src/main.rs
@@ -1,57 +1,79 @@
 use std::collections::hashmap::HashMap;
-use std::io;
 
+#[cfg(not(test))]
 fn main() {
-  println!("Input a string: ");
-  let input = io::stdin()
-                .read_line()
-                .ok()
-                .expect("Failed to read input.");
-  println!("First non-repeated character: {}", first_nonrepeated_character(input.as_slice()));
+  let input = first_nonrepeated_character("ABA");
+  match input {
+    Some(val) => { assert_eq!("B", val)},
+    None => assert!(false)
+  }
 }
 
-fn first_nonrepeated_character(input_str: &str) -> char {
-  let mut char_hash:HashMap<char, int> = HashMap::new();
 
-  for c in input_str.chars() {
-    let is_none = match char_hash.find_mut(&c) {
-      None => true,
-      Some(val) => { *val += 1; false }
-    };
-
-    if is_none {
-      char_hash.insert(c, 1i);
-    }
+#[cfg(not(test))]
+// our function returns an option, which encapsulates the fact
+// that it may return a valid char or 'None' otherwise
+fn first_nonrepeated_character(input: &str) -> Option<char> {
+  // instantiate a hash map to store key, value pairs
+  let mut char_hash: HashMap<char, uint> = HashMap::new();
+  // for each character in the input string
+  for character in input.chars() {
+    // either insert a character that isn't already in the hash map
+    // or update the value associated with each key, which keeps a 
+    // count of how many times each char has appeared so far
+    char_hash.insert_or_update_with(character, 1, |_key, val| *val += 1 );
   }
-
-  let answer: char = '-';
-
-  for (k, v) in char_hash.iter() {
-    if *v == 1 {
-      let answer = k;
-      return *answer;
+  // loop through each character in input string again
+  for character in input.chars() {
+    // this time we check each character in the string against its running count value
+    // if its count value equals 1
+    if char_hash[character] == 1u {
+      // it is our first non-repeated character
+      return Some(character);
     }
-  }
-  answer
+  } 
+  // otherwise None is returned
+  None
+
 }
 
 #[test]
 fn first_character_non_repeated() {
-  assert!(first_nonrepeated_character("ABBCCC") == "A");
+  let test: &str = "ABBCCC";
+  let answer = first_nonrepeated_character(test);
+  match answer {
+    Some(val) => assert_eq!(val, "A"),
+    None => assert!(false)
+  }
 }
 
 #[test]
 fn middle_character_non_repeated() {
-  assert!(first_nonrepeated_character("AABCCD") == "B");
+  let test: &str = "AABCCD";
+  let answer = first_nonrepeated_character(test);
+  match answer {
+    Some(val) => assert_eq!(val, "B"),
+    None => assert!(false)
+  }
 }
 
 #[test]
 fn last_character_non_repeated() {
-  assert!(first_nonrepeated_character("ACABACBD") == "D");
+  let test: &str = "ACABACBD";
+  let answer = first_nonrepeated_character(test);
+  match answer {
+    Some(val) => assert_eq!(val, "D"),
+    None => assert!(false)
+  }
 }
 
 #[test]
 fn no_repeated_characters() {
-  assert!(first_nonrepeated_character("ABCD") == "-");
+  let test: &str = "ABCD";
+  let answer = first_nonrepeated_character(test);
+  match answer {
+    Some(val) => assert!(false),
+    None => assert!(true)
+  }
 }
 
