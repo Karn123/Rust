db0536893dbfabb986522e1394864d5a5e241bdb
Sean Chen <skypemaster007@gmail.com>
2014-09-18 16:58:40 -0700

    Refactored is_subset_of to work correctly and pass all tests

--- is_subset_of/src/main.rs
+++ is_subset_of/src/main.rs
@@ -1,18 +1,43 @@
-use std::collections::hashmap::HashMap;
+use std::collections::HashSet;
+use std::hash::Hash;
 
 fn main() {
-  println!(is_subset_of(["commit", "push"], ["commit", "push", "rebase", "push", "blame"]));
+  println!("The first array is a subset of the second array: {}", is_subset_of(["commit", "push"], ["commit", "push", "rebase", "push", "blame"]));
 }
 
-fn is_subset_of(first_arr: [&str], second_arr: [&str]) -> bool {
-  let mut hash: HashMap<&str, uint> = HashMap::new();
+fn is_subset_of<T: Eq + Hash>(first_arr: &[T], second_arr: &[T]) -> bool {
+  let mut set: HashSet<&T> = HashSet::new();
   for i in second_arr.iter() {
-    hash.insert_or_update_with( *i, 1, |_key, val| *val += 1 );
+    set.insert(i);
   }
   for i in first_arr.iter() {
-    if hash[*i] < 1u {
+    if !set.contains(&i) {
       return false;
     }
   }
-  return true;
+  true
+}
+
+#[test]
+fn test_one() {
+  let result = is_subset_of(["commit", "push"], ["rebase", "blame", "origin", "master"]);
+  assert_eq!(result, false);
+}
+
+#[test]
+fn test_two() {
+  let result = is_subset_of(["commit", "push"], ["rebase", "blame", "origin", "master", "push", "commit"]);
+  assert_eq!(result, true);
+}
+
+#[test]
+fn test_three() {
+  let result = is_subset_of(["commit", "push"], ["commit"]);
+  assert_eq!(result, false);
+}
+
+#[test]
+fn test_four() {
+  let result = is_subset_of([1u, 2, 3], [3u, 4, 1, 2]);
+  assert_eq!(result, true);
 }
